46c87857eda6e4562390729c82377d9c317b4567
mohamed3b3az <mohamedabdelaziem96@gmail.com>
2024-04-24 21:59:33 +0200

    Decoder

--- AES.v
+++ AES.v
@@ -43,3 +43,87 @@ module inverse_shift_Rows(input [127:0] state ,output wire [127:0] out);
     assign out[95:88]=state[127:120];
     assign out[127:120]=state[31:24];
 endmodule
+
+module decoder(input[2:0] word,output wire[6:0]HEX0,output wire[6:0]HEX1,output wire[6:0] HEX2);
+reg HEX0_1[6:0];
+reg HEX0_2[6:0];
+reg HEX0_3[6:0];    
+always @(*) 
+begin 
+if(word[0]==0)
+     HEX0_1<=7'b1000000;
+else if(word[0]==1)
+     HEX0_1<=7'b1111001;
+else if(word[0]==2)
+     HEX0_1<=7'b0100100;
+else if(word[0]==3)
+     HEX0_1<=7'b0110000;
+else if(word[0]==4)
+     HEX0_1<=7'b0011001;
+else if(word[0]==5)
+     HEX0_1<=7'b0010010;
+else if(word[0]==6)
+     HEX0_1<=7'b0000010;
+else if(word[0]==7)
+     HEX0_1<=7'b0111000;
+else if(word[0]==8)
+     HEX0_1<=7'b0000000;
+else if(word[0]==9)
+     HEX0_1<=7'b0010000;
+else 
+     HEX0_1<=7'b1111111;
+end
+always @(*) 
+begin 
+if(word[1]==0)
+     HEX0_2<=7'b1000000;
+else if(word[1]==1)
+     HEX0_2<=7'b1111001;
+else if(word[1]==2)
+     HEX0_2<=7'b0100100;
+else if(word[1]==3)
+     HEX0_2<=7'b0110000;
+else if(word[1]==4)
+     HEX0_2<=7'b0011001;
+else if(word[1]==5)
+     HEX0_2<=7'b0010010;
+else if(word[1]==6)
+     HEX0_2<=7'b0000010;
+else if(word[1]==7)
+     HEX0_2<=7'b0111000;
+else if(word[1]==8)
+     HEX0_2<=7'b0000000;
+else if(word[1]==9)
+     HEX0_2<=7'b0010000;
+else 
+     HEX0_2<=7'b1111111;
+end
+always @(*) 
+begin 
+if(word[2]==0)
+     HEX0_3<=7'b1000000;
+else if(word[2]==1)
+     HEX0_3<=7'b1111001;
+else if(word[2]==2)
+     HEX0_3<=7'b0100100;
+else if(word[2]==3)
+     HEX0_3<=7'b0110000;
+else if(word[2]==4)
+     HEX0_3<=7'b0011001;
+else if(word[2]==5)
+     HEX0_3<=7'b0010010;
+else if(word[2]==6)
+     HEX0_3<=7'b0000010;
+else if(word[2]==7)
+     HEX0_3<=7'b0111000;
+else if(word[2]==8)
+     HEX0_3<=7'b0000000;
+else if(word[2]==9)
+     HEX0_3<=7'b0010000;
+else 
+     HEX0_3<=7'b1111111;
+end
+assign HEX0=HEX0_1;
+assign HEX1=HEX0_2;
+assign HEX2=HEX0_3;
+endmodule
