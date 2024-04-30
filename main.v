766afd702322275100356fdd5d73470c87e1f989
mohamed3b3az <mohamedabdelaziem96@gmail.com>
2024-04-30 07:29:48 +0300

    update  main with encoder and decoder

--- main.v
+++ main.v
@@ -1,15 +1,19 @@
-module main(input[2:0]SW,output wire HEX0,output wire HEX1,output wire HEX2,input clk,output wire [127:0]out_main,output wire [127:0] out_desipher);
+module main(input[2:0]SW,output wire[6:0] HEX0,output wire[6:0] HEX1,output wire[6:0] HEX2,input clk,output wire [127:0]out_main,output wire [127:0] out_desipher);
 integer i=0;
 integer j=0;
+//decipher
 reg  [127:0]in_disipher;
 wire [127:0]main_disipher_input;
+//cipher 128
 wire [127:0] state;
 wire [127:0] temp_out_main_cipher;
 wire [127:0] temp_out_main_decipher;
 wire [127:0] key_128;
 reg  [127:0] bit128;
 wire [127:0] state_128;
+//encoder
 wire [7:0] encoder_input;
+//decoder
 wire [11:0]input_decoder;
 // reg  [127:0] bit192;
 // wire [127:0] state_192;
@@ -32,9 +36,9 @@ Cipher128 c(temp_out_main_cipher,clk,state_128,key_128);
 assign out_main=temp_out_main_cipher;
 InvCipher128 s(temp_out_main_decipher,clk,main_disipher_input,key_128);
 assign out_desipher=temp_out_main_decipher;
-  
+assign encoder_input=out_main;
     reg [11:0]bcd;
-     always @(encoder_input)
+     always @(posedge clk)
         begin
             bcd = 0; //initialize bcd to zero.
             for (j = 0; j < 8; j = j+1) //run for 8 iterations
@@ -50,6 +54,95 @@ assign out_desipher=temp_out_main_decipher;
                     bcd[11:8] = bcd[11:8] + 3;  
             end
         end    
-    assign input_decoder=bcd; //assign the output
-    
+assign input_decoder=bcd; //assign the output
+reg [6:0] HEX0_1;
+reg [6:0] HEX0_2;
+reg [6:0] HEX0_3;    
+always @(posedge clk) 
+begin 
+    if(input_decoder!== 'bx)
+    begin
+if(input_decoder[3:0]==0)
+     HEX0_1<=7'b1000000;
+else if(input_decoder[3:0]==1)
+     HEX0_1<=7'b1111001;
+else if(input_decoder[3:0]==2)
+     HEX0_1<=7'b0100100;
+else if(input_decoder[3:0]==3)
+     HEX0_1<=7'b0110000;
+else if(input_decoder[3:0]==4)
+     HEX0_1<=7'b0011001;
+else if(input_decoder[3:0]==5)
+     HEX0_1<=7'b0010010;
+else if(input_decoder[3:0]==6)
+     HEX0_1<=7'b0000010;
+else if(input_decoder[3:0]==7)
+     HEX0_1<=7'b0111000;
+else if(input_decoder[3:0]==8)
+     HEX0_1<=7'b0000000;
+else if(input_decoder[3:0]==9)
+     HEX0_1<=7'b0010000;
+else 
+     HEX0_1<=7'b1111111;
+    end
+end
+always @(posedge clk) 
+begin 
+    if(input_decoder!== 'bx)
+    begin
+if(input_decoder[7:4]==0)
+     HEX0_2<=7'b1000000;
+else if(input_decoder[7:4]==1)
+     HEX0_2<=7'b1111001;
+else if(input_decoder[7:4]==2)
+     HEX0_2<=7'b0100100;
+else if(input_decoder[7:4]==3)
+     HEX0_2<=7'b0110000;
+else if(input_decoder[7:4]==4)
+     HEX0_2<=7'b0011001;
+else if(input_decoder[7:4]==5)
+     HEX0_2<=7'b0010010;
+else if(input_decoder[7:4]==6)
+     HEX0_2<=7'b0000010;
+else if(input_decoder[7:4]==7)
+     HEX0_2<=7'b0111000;
+else if(input_decoder[7:4]==8)
+     HEX0_2<=7'b0000000;
+else if(input_decoder[7:4]==9)
+     HEX0_2<=7'b0010000;
+else 
+     HEX0_2<=7'b1111111;
+end
+end
+always @(posedge clk) 
+begin 
+    if(input_decoder!== 'bx)
+    begin
+if(input_decoder[11:8]==0)
+     HEX0_3<=7'b1000000;
+else if(input_decoder[11:8]==1)
+     HEX0_3<=7'b1111001;
+else if(input_decoder[11:8]==2)
+     HEX0_3<=7'b0100100;
+else if(input_decoder[11:8]==3)
+     HEX0_3<=7'b0110000;
+else if(input_decoder[11:8]==4)
+     HEX0_3<=7'b0011001;
+else if(input_decoder[11:8]==5)
+     HEX0_3<=7'b0010010;
+else if(input_decoder[11:8]==6)
+     HEX0_3<=7'b0000010;
+else if(input_decoder[11:8]==7)
+     HEX0_3<=7'b0111000;
+else if(input_decoder[11:8]==8)
+     HEX0_3<=7'b0000000;
+else if(input_decoder[11:8]==9)
+     HEX0_3<=7'b0010000;
+else 
+     HEX0_3<=7'b1111111;
+    end
+end
+assign HEX0=HEX0_1;
+assign HEX1=HEX0_2;
+assign HEX2=HEX0_3;
 endmodule
