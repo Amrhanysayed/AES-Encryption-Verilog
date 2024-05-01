726a731fb86ae32b8e6f9a6eedd963a5b341ee08
Amr Samy <41027803+AmrSamy59@users.noreply.github.com>
2024-05-01 04:41:53 +0300

    AES192

--- AES192.v
+++ AES192.v
@@ -1,14 +1,17 @@
-module Cipher128(output wire [127:0] out1,input clk,input [127:0]state,input [127:0]key);
+module Cipher192(output wire [127:0] out1,input clk);//, input [127:0]state, input [191:0] key);
     reg  [127:0] state0;
-    reg  [127:0]temp;
-    // wire [127:0] key;
+    reg  [127:0] temp;
     wire [127:0] out;
     wire [127:0] out_lastround;
     integer i=-1;
-    //assign state = 128'h00112233445566778899aabbccddeeff;
-    //assign key = 128'h000102030405060708090a0b0c0d0e0f;
-    wire [0:1407] w;
-    KeyExpansion128 uut(key, w);
+
+    // for testing
+    wire [127:0] state;
+    wire [191:0] key;
+    assign state = 128'h00112233445566778899aabbccddeeff;
+    assign key = 192'h000102030405060708090a0b0c0d0e0f1011121314151617;
+    wire [0:1663] w;
+    KeyExpansion192 uut(key, w);
     round x(state0, w[((i+1)*128)+:128],out);   
  always@ (posedge clk) 
     begin 
@@ -18,13 +21,13 @@ module Cipher128(output wire [127:0] out1,input clk,input [127:0]state,input [12
             temp<=out;
             i=i+1;
         end
-        else if(i < 9 && state !== 'bx)
+        else if(i < 11 && state !== 'bx)
         begin
         state0<=out;  
         temp<=out;
         i=i+1;
         end
-        else if(i == 9 && state !== 'bx)
+        else if(i == 11 && state !== 'bx)
         begin
             temp<=out_lastround;
             i=i+1;
@@ -35,25 +38,30 @@ module Cipher128(output wire [127:0] out1,input clk,input [127:0]state,input [12
     //assign temp=out_lastround;
 endmodule
 
-module InvCipher128(output [127:0] out1,input clk,input [127:0]state,input [127:0]key);
+module InvCipher192(output wire [127:0] out1,input clk); //, input [127:0]state, input [191:0] key);
     reg  [127:0] state0;
-    reg  [127:0]temp;
+    reg  [127:0] temp;
     wire [127:0] out;
     wire [127:0] out_lastround;
-    //assign state = 128'h69c4e0d86a7b0430d8cdb78070b4c55a;
-   // assign key = 128'h000102030405060708090a0b0c0d0e0f;
-    integer i=10;
+
+    // for testing
+    wire [191:0] key;
+    wire  [127:0] state;
+    
+    assign state = 128'hdda97ca4864cdfe06eaf70a0ec0d7191;
+    assign key = 192'h000102030405060708090a0b0c0d0e0f1011121314151617;
+    integer i=12;
 
     
-    wire [0:1407] w;
+    wire [0:1663] w;
 
-    KeyExpansion128 uut(key, w);
+    KeyExpansion192 uut(key, w);
     round_inverse r(state0,w[((i)*128)+:128],out);
     always@ (posedge clk) 
     begin 
-        if(i==10 && state !== 'bx)
+        if(i == 12 && state !== 'bx)
         begin
-            state0<=state^w[1280+:128];
+            state0<=state^w[1536+:128];
             temp<=out;
             i=i-1;
         end
@@ -75,19 +83,19 @@ module InvCipher128(output [127:0] out1,input clk,input [127:0]state,input [127:
 
 endmodule
 
-module Cipher128_tb(); //must run in modelsim ( run 1200 )
+module Cipher192_tb(); // must run in modelsim ( run 1400 )
 
     reg clk; // Define clock signal
     wire [127:0] _output;
 
     // Instantiate the module under test
-    Cipher128 uut(_output, clk);
+    Cipher192 uut(_output, clk);
 
     // Clock generation
     initial begin
         clk = 0; // Initialize clock
         #50;
-        repeat (10) begin // Repeat for 10 positive edges
+        repeat (13) begin // Repeat for 11 positive edges
             #50 clk = 1; // Set clock high after 50 time units
             #50 clk = 0; // Set clock low after another 50 time units
         end
@@ -101,18 +109,18 @@ endmodule
 
 
 
-module InvCipher128_tb();
+module InvCipher192_tb(); // must run in modelsim ( run 1400 )
     reg clk; // Define clock signal
     wire [127:0] _output;
 
     // Instantiate the module under test
-    InvCipher128 uut(_output, clk);
+    InvCipher192 uut(_output, clk);
 
     // Clock generation
     initial begin
         clk = 0; // Initialize clock
         #50;
-        repeat (10) begin // Repeat for 10 positive edges
+        repeat (13) begin // Repeat for 10 positive edges
             #50 clk = 1; // Set clock high after 50 time units
             #50 clk = 0; // Set clock low after another 50 time units
         end
