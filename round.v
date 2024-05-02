1232b562c1b6fd3b0af336999bc74c90d95c9755
mohamed3b3az <mohamedabdelaziem96@gmail.com>
2024-05-02 18:04:38 +0300

    deleting comments

--- round.v
+++ round.v
@@ -5,8 +5,5 @@ ShiftRows w1(states[0],states[1]);
 MixColumns w2(states[1],states[2]);
 AddRoundKey q(states[2],key,states[3]);
 assign out=states[3];
-//wire [6:0]out_deoder[2:0];
-//wire [11:0]out_endoer;
-//Encoder w25(states[3][7:0],out_endoer);
-//Decoder w7(out_endoer,out_deoder[0],out_deoder[1],out_deoder[2]);
+
 endmodule
