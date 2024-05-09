721b7aa982a308fe8a651e7712ab4f3c003c0623
Amr Samy <41027803+AmrSamy59@users.noreply.github.com>
2024-05-09 14:41:39 +0300

    Revert "Version1.3"
    This reverts commit 8deaffae336c3f7246b354aff950ad13e7ab013f.

--- Encrypt.v
+++ Encrypt.v
@@ -1,11 +1,20 @@
 module Encrypt  #(parameter nk=4,parameter nr=10) (input  [(nk*32)-1:0] key,input clk, input  reset,input [127:0]state,input [0:((nr+1)*128)-1] w,output wire [127:0] out1);
+    //;
+    //wire [127:0] state;
     reg  [127:0] state0;
     reg  [127:0]temp;
+    // wire [127:0] key;
     wire [127:0] out;
     wire [127:0] out_lastround;
     integer i=-1;
+    //// for testing only /////////////////////////////////////////
+    // assign state = 128'h00112233445566778899aabbccddeeff;
+    // assign key = 256'h000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f;
+    //////////////////////////////////////////////////////////////
+    
+   // keyExpansion #(nk, nr) key_expansion_inst (key,w);
     round x(state0, w[((i+1)*128)+:128],out);   
- always@ (posedge clk or posedge reset or negedge reset)
+ always@ (posedge clk or posedge reset)
     begin 
         if(reset==1'b1)
         begin
@@ -36,5 +45,6 @@ module Encrypt  #(parameter nk=4,parameter nr=10) (input  [(nk*32)-1:0] key,inpu
     end
     assign out1=temp;
     last_round xs(state0,w[((i+1)*128)+:128],out_lastround);
+    //assign temp=out_lastround;
 endmodule
 
